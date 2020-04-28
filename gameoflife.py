91830f2652905b0c2d4c9747b777c51706c3df5f
Ender <enderredinmc@gmail.com>
2020-04-28 14:30:12 +0200

    Updates

--- gameoflife.py
+++ gameoflife.py
@@ -2,29 +2,42 @@ import numpy as np      # import NumPy
 import time             # import Time
 import pygame as pg     # import PyGame
 import math             # import Math
+pg.font.init()          # Initialize font module from pygame
 
-width = 1000; height = 1000                 # Establish width and height of the window
+width = 1000; height = 1000                 # Establish width and height of the grid
 bg = (35, 35, 35)                           # Establish background color of window
-border = 1; color = (125, 125, 125)         # Establish color and border size of cells
+border = 1; color = (int(width/8), int(width/8), int(width/8))         # Establish color and border size of cells
 celX = 75; celY = 75                        # Establish number of cell in each dimension
 celw = width / celX; celh = width / celY    # Establish the width and height of a cell depending on the size of the window and the number of cells
 pause = 1                                   # Establish the boolean variable pause, which is used to determine whether the game is paused or not
 kill = 0                                    # Declare the kill boolean variable, if set to TRUE, the program will end
-i=0                                         # Declare i as the number of iterations of the game
+i = 0                                       # Declare i as the number of iterations of the game
+gameState = np.zeros((celX,celY))           # Establishing the matrix gameState which contains the values of all cells, its dimensions are defined with the number of cells
 
-screen = pg.display.set_mode([width + border,height + border])                                          # Set up the display with size defined by width and height, plus the border so when cells are drawn it does not get cut off the edges
+
+screen = pg.display.set_mode([width + border + int(width/4),height + border])                           # Set up the display with size defined by width and height of the grid, plus the border so when cells are drawn it does not get cut off the edges and plus a fourth of the width to display text
+font = pg.font.SysFont(None, int(height/18))                    # We set up font 1 for texts, its size depends on the height of the gird. BIG
+font2 = pg.font.SysFont(None, int(height/25))                   # We set up font 2 for texts, its size depends on the height of the gird. MEDIUM
+font3 = pg.font.SysFont(None, int(height/40))                   # We set up font 3 for texts, its size depends on the height of the gird. SMALL
+text_game = font.render("Game",1,color)                         # TEXT FOR "Game"           color "color"   antialias = 1
+text_of = font.render("of",1,color)                             # TEXT FOR "of"             color "color"   antialias = 1
+text_life = font.render("Life",1,color)                         # TEXT FOR "Life"           color "color"   antialias = 1
+text_paused = font.render("Paused",1,color)                     # TEXT FOR "Paused"         color "color"   antialias = 1
+text_running = font.render("Running",1,color)                   # TEXT FOR "Running"        color "color"   antialias = 1
+text_it = font.render("Iteration:",1,color)                     # TEXT FOR "Iteration"      color "color"   antialias = 1
+text_cellst = font.render("Living Cells:",1,color)              # TEXT FOR "Living cells"   color "color"   antialias = 1
+text_ins1 = font3.render("LClick - Turn alive",1,color)         # TEXT FOR Instruc1         color "color"   antialias = 1
+text_ins2 = font3.render("RClick - Kill cell",1,color)          # TEXT FOR Instruc2         color "color"   antialias = 1
+text_ins3 = font3.render("SPACE - Pause/Unpause",1,color)       # TEXT FOR Instruc3         color "color"   antialias = 1
+text_ins4 = font3.render("Q/W - Increase X/Y of grid",1,color)  # TEXT FOR Instruc4         color "color"   antialias = 1
+text_ins5 = font3.render("A/S - Decrease X/Y of grid",1,color)  # TEXT FOR Instruc5         color "color"   antialias = 1
+text_ins6 = font3.render("BACKSPACE - Reset",1,color)           # TEXT FOR Instruc6         color "color"   antialias = 1
+text_ins7 = font3.render("ESC - End Game",1,color)              # TEXT FOR Instruc7         color "color"   antialias = 1
 pg.display.set_caption("GAME OF LIFE"); icon = pg.image.load("ICON.ico"); pg.display.set_icon(icon)     # Set up the window title and icon
 screen.fill(bg)                                                                                         # Coloring the background with the colour established in bg
-gameState = np.zeros((celX,celY))                                                                       # Establishing the matrix gameState which contains the values of all cells, its dimensions are defined with the number of cells
+
 while 1:                                                                            # Main loop of the game, if it ends (and the only way is through a break statement), the game ends
-    if not pause:                                                                   # Checks if the game is not paused
-        time.sleep(4/math.sqrt(celX*celY))                                          # Creates a delay proportional to the number of cells
-        pg.display.set_caption("GAME OF LIFE - " + str(celX) + "x" + str(celY) + " - RUNNING - " + str(int(gameState.sum())) + " CELLS - ITERATION " + str(i))  # Changes the window title to show relevant information, such as the number of cells, whether it's paused or running, the number of living cells and the iteration
-    else:                                                                           # If the game is not unpaused, it's paused
-        pg.display.set_caption("GAME OF LIFE - " + str(celX) + "x" + str(celY) + " - PAUSED - " + str(int(gameState.sum())) + " CELLS - ITERATION " + str(i))   # Changes the window title to show relevant information, such as the number of cells, whether it's paused or running, the number of living cells and the iteration
-    ev = pg.event.get()                                                             # Get events from PyGame, inputs. Storing it into a variable
-    mouse = pg.mouse.get_pressed()                                                  # Get which buttons of the mouse are pressed, storing it into a tuple
-    for event in ev:                                                                # Check for every event
+    for event in pg.event.get():                                                    # Check for every event recorded by pygame
         if event.type == pg.KEYDOWN and event.key == pg.K_SPACE: pause = not pause  # If the space bar is pressed, the value of game changes, pausing or unpausing the game
         if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE: kill = 1          # If the ESC key is pressed, the value if kill changes to TRUE
         if event.type == pg.KEYDOWN and event.key == pg.K_BACKSPACE:                # If the backspace key is pressed, the game resets:
@@ -41,21 +54,51 @@ while 1:                                                                        
         if event.type == pg.KEYDOWN and event.key == pg.K_s:                        # If S key is pressed:
             celY -= 1; celh = width / celY                                          # Number of cells in Y decreases and we recalcule the height of cells
             gameState = np.zeros((celX,celY)); i = 0; pause = 1                     # Resets the game
-        if mouse[0] == 1:                                                           # If pressing the left button of the mouse:
+        if pg.mouse.get_pressed()[0] == 1:                                          # If pressing the left button of the mouse, which is stored in a tuple of 3 coordinates, position 0:
             cursor = pg.mouse.get_pos()                                             # Get the coordinates of the cursor
             gameState[int(cursor[0]/celw),int(cursor[1]/celh)] = 1                  # Change to 1 (alive) the state of the cell in those coordinates, for that, we divide the coordinates between the size of a cell and then take the integer part
-        if mouse[2] == 1:                                                           # If pressing the right button of the mouse:
+        if pg.mouse.get_pressed()[2] == 1:                                          # If pressing the right button of the mouse, which is stored in a tuple of 3 coordinates, position 2:
             cursor = pg.mouse.get_pos()                                             # Get the coordinates of the cursor
             gameState[int(cursor[0]/celw),int(cursor[1]/celh)]  = 0                 # Change to 0 (dead) the state of the cell in those coordinates, for that, we divide the coordinates between the size of a cell and then take the integer part
     if kill: break                                                                  # If kill is TRUE, the while loop breaks and the program ends
+
     newgameState = np.copy(gameState)                                               # We make a copy of the game matrix in which we are going to make the changes each generation so we do not overwrite gameState while we are reading it
     screen.fill(bg)                                                                 # We fill the screen with the background to clean everything
+
+    text_i = font2.render(str(i),1,color)                           # TEXT FOR number of iterations         color "color"   antialias = 1
+    text_cells = font2.render(str(int(gameState.sum())),1,color)    # TEXT FOR number of liivng cells       color "color"   antialias = 1
+    text_dim = font.render(str(celX) + " x " + str(celY),1,color)   # TEXT FOR dimension of the grid        color "color"   antialias = 1
+    screen.blit(text_game, (width + border + int(width/8) - text_game.get_width() // 2, int(height/20)*1 - text_game.get_height() // 2))
+    screen.blit(text_of, (width + border + int(width/8) - text_of.get_width() // 2, int(height/20)*2 - text_of.get_height() // 2))
+    screen.blit(text_life, (width + border + int(width/8) - text_life.get_width() // 2, int(height/20)*3 - text_life.get_height() // 2))
+    screen.blit(text_dim, (width + border + int(width/8) - text_dim.get_width() // 2, int(height/20)*6 - text_dim.get_height() // 2))
+    screen.blit(text_cellst, (width + border + int(width/8) - text_cellst.get_width() // 2, int((height/20)*7.5) - text_cellst.get_height() // 2))
+    screen.blit(text_cells, (width + border + int(width/8) - text_cells.get_width() // 2, int((height/20)*8.5) - text_cells.get_height() // 2))
+    screen.blit(text_it, (width + border + int(width/8) - text_it.get_width() // 2, int((height/20)*9.5) - text_it.get_height() // 2))
+    screen.blit(text_i, (width + border + int(width/8) - text_i.get_width() // 2, int((height/20)*10.5) - text_i.get_height() // 2))
+    screen.blit(text_ins1, (width + border + int(width/8) - text_ins1.get_width() // 2, int((height/20)*15) - text_ins1.get_height() // 2))
+    screen.blit(text_ins2, (width + border + int(width/8) - text_ins2.get_width() // 2, int((height/20)*15.5) - text_ins2.get_height() // 2))
+    screen.blit(text_ins3, (width + border + int(width/8) - text_ins3.get_width() // 2, int((height/20)*16) - text_ins3.get_height() // 2))
+    screen.blit(text_ins4, (width + border + int(width/8) - text_ins4.get_width() // 2, int((height/20)*16.5) - text_ins4.get_height() // 2))
+    screen.blit(text_ins5, (width + border + int(width/8) - text_ins5.get_width() // 2, int((height/20)*17) - text_ins5.get_height() // 2))
+    screen.blit(text_ins6, (width + border + int(width/8) - text_ins6.get_width() // 2, int((height/20)*17.5) - text_ins6.get_height() // 2))
+    screen.blit(text_ins7, (width + border + int(width/8) - text_ins7.get_width() // 2, int((height/20)*18) - text_ins1.get_height() // 2))
+    # screen.blit render all the different texts in a relative centered position position to the right of the grid
+
+    if not pause:                                                                   # Checks if the game is not paused
+        time.sleep(2/math.sqrt(celX*celY))                                          # Creates a delay proportional to the number of cells
+        pg.display.set_caption("GAME OF LIFE - " + str(celX) + "x" + str(celY) + " - RUNNING - " + str(int(gameState.sum())) + " CELLS - ITERATION " + str(i))  # Changes the window title to show relevant information, such as the number of cells, whether it's paused or running, the number of living cells and the iteration
+        screen.blit(text_running, (width + border + int(width/8) - text_running.get_width() // 2, int((height/20)*4.5) - text_running.get_height() // 2))       # render the RUNNING text
+    else:                                                                           # If the game is not unpaused, it's paused
+        pg.display.set_caption("GAME OF LIFE - " + str(celX) + "x" + str(celY) + " - PAUSED - " + str(int(gameState.sum())) + " CELLS - ITERATION " + str(i))   # Changes the window title to show relevant information, such as the number of cells, whether it's paused or running, the number of living cells and the iteration
+        screen.blit(text_paused, (width + border + int(width/8) - text_paused.get_width() // 2, int((height/20)*4.5) - text_paused.get_height() // 2))          # render the PAUSE text
     for x in range(0,celX):                                                         # Two for loops x and y for the number of cells y each dimension, x and y are the coordinates of the cells. A series of operations are going to happen for each pair of coordinates
         for y in range(0, celY):
             poly = [(round(x*celw         ), round(y*celh        )),                # This list of points contains the ordered vertices of a square, which will define the shape of the cell.
                     (round(x*celw + celw  ), round(y*celh        )),                # Each vertex is calculated multiplying the cell coordinate with the cell width or height
                     (round(x*celw + celw  ), round(y*celh + celh )),                # and then adding the height or the width depending on the vertex
                     (round(x*celw         ), round(y*celh + celh ))]
+
             if not pause:                                                           # If the game is paused, the operations for updating the game will not happen, and the gameState will not change
                 nvalue = gameState[(x+1) % celX , (y+1) % celY  ] + \
                          gameState[(x)   % celX , (y+1) % celY  ] + \
@@ -65,11 +108,14 @@ while 1:                                                                        
                          gameState[(x)   % celX , (y-1) % celY  ] + \
                          gameState[(x+1) % celX , (y-1) % celY  ] + \
                          gameState[(x+1) % celX , (y)   % celY  ]  # To know how many living neighbours a cell has, we sum the value of the 8 cells surrounding the cell (excluding it) in the matrix, if the cell is in the edge, the modulus operation will output the cell on the other side, behaving like a torus. For example, in (5,74), if there are 75 cells and it begins counting with 0, when checking for (5,75), it will check for (5,0).
+
                 if gameState[x,y] == 0 and nvalue == 3: newgameState[x,y] = 1                   # If the cell is dead (gameState == 0) and has exactly 3 living neighbours, it becomes alive. The changes are made in newgameState
                 elif gameState[x,y] == 1 and (nvalue < 2 or nvalue > 3): newgameState[x,y] = 0  # If the cell is alive (gameState == 1) and has lees than 2 or more than three living neighbours, it will die. The changes are made in newgameState
+
                 if x == celX-1 and y == celY-1:                                     # If this is the last cell:
                     i += 1                                                          # Add 1 to i to denote that one generation or iteration has passed
             pg.draw.polygon(screen, color, poly, border)                                    # Draw the cell grid for each cell, with a defined border (not 0, because that's fill) and a defined colour     These two always happen even if the game is paused
             if not newgameState[x,y] == 0: pg.draw.polygon(screen, color, poly, 0)          # If the cell also is alive (or not dead), the square will be filled (border = 0)                               These two always happen even if the game is paused
+
     gameState = newgameState                        # save the changes made in newgameState to gameState
     pg.display.flip()                               # update all the changes on the screen
